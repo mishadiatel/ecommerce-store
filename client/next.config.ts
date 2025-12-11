acc16830e4d5d094d9e68ea3e66035132007cae6
Mykhailo Diatel <mishadiatel13@gmail.com>
2025-12-11 13:53:19 +0200

    refacotor: remove unused get api url from next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,6 @@
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const apiUrl = process.env.NEXT_PUBLIC_PROJECT_API_URL!;
-const { protocol, hostname } = new URL(apiUrl);
-
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
